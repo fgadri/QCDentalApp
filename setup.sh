fa528bc9af043415bb4548d1fe307d5f154e0881
JoshuaRabino <joshrabino23@gmail.com>
2015-03-07 14:38:17 -0700

    Still lots of work on building dev environment -- figuring out easiest way to do this.

--- setup.sh
+++ setup.sh
@@ -6,8 +6,16 @@ NC='\033[0m' # No Color
 
 echo -e "${red}Hello CS 4690 -- Let's start building your VM...${NC}"
 
-apt-get install apache2 git php5 mysql -y
-mkdir /repo
-git clone https://github.com/scottalanweber/QCDentalApp.git /repo
-ln -s /repo/src/* /var/www/html
-rm /var/www/html/index.html
+#apt-get install git apache2 git php5 -y
+# Install dependencies
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+sudo apt-get update
+sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 php5 git
+
+
+#mkdir /repo
+cd /var/www/html
+git clone https://github.com/scottalanweber/QCDentalApp.git
+#ln -s /repo/production /var/www/html
+#rm -f /var/www/html/index.html
