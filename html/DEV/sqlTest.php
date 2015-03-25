41daa33975091983ec6015fc7d604946cc85d858
JoshuaRabino <joshrabino23@gmail.com>
2015-03-24 23:18:37 -0600

    File management, sqlTest

--- html/DEV/sqlTest.php
+++ html/DEV/sqlTest.php
@@ -1,1 +1,14 @@
-this is a sqltets
+<?php
+$servername = "localhost";
+$username = "qcadmin";
+$password = "J2PKY!HrkYt*AB8dhz@rYw\$X";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
