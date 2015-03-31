4a6a8e4986d26f9a73d7c8224d74441b0a6482a5
Brad Soderman <brad.soderman@gmail.com>
2015-03-31 12:00:07 -0600

    Stylized the views for the license controller
    https://app.asana.com/0/28637613809433/29693043507690

--- html/application/views/formSuccess.php
+++ html/application/views/formSuccess.php
@@ -5,8 +5,8 @@
 
 	<div class="container">
 		<body>
-			<h3>Your form was successfully submitted!</h3>
-			<p>Thank you for submitting your information regarding renewing your licenses. You will recieve a receive a response as soon as possible. Thank you.</p>
+			<h3 class="text-center">Your form was successfully submitted!</h3>
+			<p class="text-center">Thank you for submitting your information regarding renewing your licenses. You will recieve a receive a response as soon as possible. Thank you.</p>
 		</body>	
 	</div>
 </html>
--- html/application/views/licenses_view.php
+++ html/application/views/licenses_view.php
@@ -1,5 +1,6 @@
 
 <div class="container">
+    <h3 class="text-center">Please provide us with your information to update your company's license.</h3>
     <?php
     //display any form validation errors
     echo validation_errors();
@@ -9,33 +10,33 @@
     ?>
     
     <label for="firstName">First Name </label>
-    <input type="text" name="firstName"> <br>
+    <input type="text" name="firstName" class="form-control"> <br>
 
     <label for="lastName">Last Name </label>
-    <input type="text" name="lastName"> <br>
+    <input type="text" name="lastName" class="form-control"> <br>
 
     <label for="companyName">Company Name </label>
-    <input type="text" name="companyName"> <br>
+    <input type="text" name="companyName" class="form-control"> <br>
 
     <label for="address">Address </label>
-    <input type="text" name="address"> <br>
+    <input type="text" name="address" class="form-control"> <br>
 
     <label for="city">City </label>
-    <input type="text" name="city"> <br>
+    <input type="text" name="city" class="form-control"> <br>
 
     <label for="state">State </label>
-    <input type="text" name="state"> <br>
+    <input type="text" name="state" class="form-control"> <br>
 
     <label for="zip">Zip Code </label>
-    <input type="text" name="zip"> <br>
+    <input type="text" name="zip" class="form-control"> <br>
 
     <label for="phone">Phone Number </label>
-    <input type="text" name="phone"> <br>
+    <input type="text" name="phone" class="form-control"> <br>
 
     <label for="email">Email Address </label>
-    <input type="text" name="email"> <br>
+    <input type="text" name="email" class="form-control"> <br>
 
-    <input type="submit" name="submit" value="Submit">
+    <input type="submit" name="submit" value="Submit" class="btn btn-default"> <br>
 
     <?php form_close(); ?>
 </div>
