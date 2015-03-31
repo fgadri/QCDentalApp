c8119af8ba00560c0bd0a7346b4a3c07abc63671
Scott Weber <scottalanweber@gmail.com>
2015-03-31 10:59:08 -0600

    Finished some changes to Ratings_dao
    Ratings can now be inserted.

--- html/application/models/rating_dao.php
+++ html/application/models/rating_dao.php
@@ -4,15 +4,60 @@ if(!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
+// *********************************************************************************
+// ***** Ratings_dao Class
+// *****
+// ***** Class that contains the Database queries related to the Teeth Ratings
+// *********************************************************************************
 class Ratings_dao extends CI_Model {
+    // *********************************************************************************
+    // ***** Insert a rating into the database
+    // *****
+    // ***** Parameters: Rating ID, Rating, Comments, Tooth ID, Rating Meta ID, Technician ID, Quality Control ID
+    // ***** Return: Boolean if insert was successful
+    // *********************************************************************************
     public function insert_rating($rating_id, $rating, $timestamp, $comments, $tooth_id, $rating_meta_id, $technician_id, $quality_control_id) {
-        $retVal = array();
-        
-        $sql = "INSERT INTO ratings (rating, timestamp, comments, tooth_id, rating_meta_id, technician_id, quality_control_id) "
-            . "VALUES (?, ?, ?, ?, ?, ?, ?) "
-            . "ON DUPLICATE KEY UPDATE "
-            . "rating = ?, "
-            . ""
+        if($rating_id === "") {
+            $sql = "INSERT INTO ratings (rating, timestamp, comments, tooth_id, rating_meta_id, technician_id, quality_control_id) "
+                ."VALUES (?, ?, ?, ?, ?, ?, ?) "
+                ."ON DUPLICATE KEY UPDATE "
+                ."rating = ?, "
+                ."timestamp = ?, "
+                ."comments = ?;";
+            $query = $this->db->query($sql, array(
+                $rating,
+                $timestamp,
+                $comments,
+                $tooth_id,
+                $rating_meta_id,
+                $technician_id,
+                $quality_control_id,
+                )
+            );
+        } else {
+            $sql = "INSERT INTO ratings (rating_id, rating, timestamp, comments, tooth_id, rating_meta_id, technician_id, quality_control_id) "
+                ."VALUES (?, ?, ?, ?, ?, ?, ?, ?) "
+                ."ON DUPLICATE KEY UPDATE "
+                ."rating = ?, "
+                ."timestamp = ?, "
+                ."comments = ?;";
+            $query = $this->db->query($sql, array(
+                $rating_id, 
+                $rating,
+                $timestamp,
+                $comments,
+                $tooth_id,
+                $rating_meta_id,
+                $technician_id,
+                $quality_control_id,
+                )
+            );
+        }
+        if($query) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
 
+}
