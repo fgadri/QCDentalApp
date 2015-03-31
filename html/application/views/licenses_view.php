2da92fd3e67bff314ad36131f1570165b590026d
Scott Weber <scottalanweber@gmail.com>
2015-03-31 11:32:19 -0600

    Made some minor changes to the licenses_view

--- html/application/views/licenses_view.php
+++ html/application/views/licenses_view.php
@@ -1,44 +1,41 @@
-<?php
 
-//display any form validation errors
-echo validation_errors();
-
-//using the form helper to help create the start of form code
-echo form_open("licenses");
-
-?>
 <div class="container">
+    <?php
+    //display any form validation errors
+    echo validation_errors();
 
-	<label for="firstName">First Name </label>
-	<input type="text" name="firstName"> <br>
+    //using the form helper to help create the start of form code
+    echo form_open("licenses");
+    ?>
+    
+    <label for="firstName">First Name </label>
+    <input type="text" name="firstName"> <br>
 
-	<label for="lastName">Last Name </label>
-	<input type="text" name="lastName"> <br>
+    <label for="lastName">Last Name </label>
+    <input type="text" name="lastName"> <br>
 
-	<label for="companyName">Company Name </label>
-	<input type="text" name="companyName"> <br>
+    <label for="companyName">Company Name </label>
+    <input type="text" name="companyName"> <br>
 
-	<label for="address">Address </label>
-	<input type="text" name="address"> <br>
+    <label for="address">Address </label>
+    <input type="text" name="address"> <br>
 
-	<label for="city">City </label>
-	<input type="text" name="city"> <br>
+    <label for="city">City </label>
+    <input type="text" name="city"> <br>
 
-	<label for="state">State </label>
-	<input type="text" name="state"> <br>
+    <label for="state">State </label>
+    <input type="text" name="state"> <br>
 
-	<label for="zip">Zip Code </label>
-	<input type="text" name="zip"> <br>
+    <label for="zip">Zip Code </label>
+    <input type="text" name="zip"> <br>
 
-	<label for="phone">Phone Number </label>
-	<input type="text" name="phone"> <br>
+    <label for="phone">Phone Number </label>
+    <input type="text" name="phone"> <br>
 
-	<label for="email">Email Address </label>
-	<input type="text" name="email"> <br>
+    <label for="email">Email Address </label>
+    <input type="text" name="email"> <br>
 
-	<input type="submit" name="submit" value="Submit">
-	
-</form>
+    <input type="submit" name="submit" value="Submit">
+
+    <?php form_close(); ?>
 </div>
-
-</html>
